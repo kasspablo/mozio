d59139ce078b8b1b7351a250aa379c263018dba7
kasspablo <kasspablo@gmail.com>
2022-03-02 22:46:56 -0300

    Configurations for Heroku

--- MozioAPI/settings/prod.py
+++ MozioAPI/settings/prod.py
@@ -1,16 +1,20 @@
 from .base import *
-
+import dj_database_url
+from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.moziogroup.herokuapp.com','.herokuapp.com']
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 # Static files (CSS, JavaScript, Images)
---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn MozioAPI.wsgi --log-file -
